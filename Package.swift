9ddcb93d2f48855e5998f4a84947a6432e9443ee
rrbox <87851278+rrbox@users.noreply.github.com>
2022-04-02 20:22:54 +0900

    構造体化

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Tsumiji",
     platforms: [
-        .macOS(.v10_10),
+        .macOS(.v12),
         .iOS(.v9)
     ],
     products: [
--- Tests/TsumijiTests/TsumijiTests.swift
+++ Tests/TsumijiTests/TsumijiTests.swift
@@ -1,11 +1,23 @@
 import XCTest
 @testable import Tsumiji
 
+extension Attribute {
+    static let red: Self = [.fontColor: NSColor.red]
+}
+
 final class TsumijiTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         
+        
+        
+        let editor = Editor()
+            .font(.red)
+            .text("Sample")
+            .fontEnd()
+        
+        print(editor.product)
     }
 }
