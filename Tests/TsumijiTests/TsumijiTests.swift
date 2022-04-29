421f41612d5411f09105dc3ceeea25c6a732d6a5
rrbox <87851278+rrbox@users.noreply.github.com>
2022-04-29 15:45:20 +0900

    Update : test

--- Tests/TsumijiTests/TsumijiTests.swift
+++ Tests/TsumijiTests/TsumijiTests.swift
@@ -6,24 +6,48 @@ extension Attribute {
     static let red: Self = [.fontColor: NSColor.red]
 }
 
+typealias NSAttribute = [NSAttributedString.Key: Any?]
+
+#if os(macOS)
+typealias Font = NSFont
+typealias Color = NSColor
+#elseif os(iOS)
+typealias Font = UIFont
+typealias Color = UIColor
+#endif
+
+extension Font {
+    static let `default` = Font(name: "HelveticaNeue-UltraLight", size: 32)
+}
+
+extension Color {
+    static let `default` = Color.white
+    static let defaultBackground = Color.clear
+}
+
 final class TsumijiTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         
-        
+        let attr = NSMutableAttributedString()
+        attr.append(NSAttributedString(string: "Sample", attributes: [
+            .font: Font.default!,
+            .foregroundColor: Color.red,
+            .backgroundColor: Color.defaultBackground
+        ]))
         
         let editor = Editor()
             .font(.red)
             .text("Sample")
             .fontEnd()
         
-        print(editor.product)
+        XCTAssertEqual(editor.product, AttributedString(attr))
         
-        let editedString: EditorLiteral = "hello\(.red), world\(.fontEnd)."
+        let editedLiteral: EditorLiteral = "\(.red)Sample\(.fontEnd)"
         
-        print(editedString.product)
+        XCTAssertEqual(editedLiteral.product, AttributedString(attr))
         
     }
 }
