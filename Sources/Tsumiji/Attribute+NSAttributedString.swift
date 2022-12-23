8fa7f4dee9a0772d6a02a792eaa207399d12de6c
rrbox <87851278+rrbox@users.noreply.github.com>
2022-12-24 01:50:20 +0900

    add AttributeLink, AttributeElement を AttributeContext に変換するメソッドを追加しました.

--- Sources/Tsumiji/Attribute+NSAttributedString.swift
+++ Sources/Tsumiji/Attribute+NSAttributedString.swift
@@ -28,6 +28,35 @@ extension AttributeContext {
         attrString.foregroundColor = self.fontColor
         attrString.font = Font(name: self.fontName, size: CGFloat(truncating: self.fontSize))
         attrString.backgroundColor = self.backgroundColor
+        
     }
 }
 
+extension AttributeElement {
+    func modify(_ context: inout AttributeContext) {
+        switch self {
+        case let .fontName(value):
+            context.fontName = value
+        case let .fontSize(value):
+            context.fontSize = value
+        case let .foregroundColor(value):
+            context.foregroundColor = value
+        case let .backgroundColor(value):
+            context.backgroundColor = value
+        }
+    }
+}
+
+extension AttributeLink {
+    func modify(_ context: inout AttributeContext) {
+        switch self {
+        case let .single(element):
+            element.modify(&context)
+            break
+        case let .link(previous, element):
+            previous.modify(&context)
+            element.modify(&context)
+            break
+        }
+    }
+}
