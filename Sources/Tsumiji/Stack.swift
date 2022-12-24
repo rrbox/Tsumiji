bbe7f8e0aa783de9aaf3624aec372a00c9fbc66b
rrbox <87851278+rrbox@users.noreply.github.com>
2022-12-24 10:39:52 +0900

    add AttributeContext-AttributeBuilder システム用の stack を作成しました.

--- Sources/Tsumiji/Stack.swift
+++ Sources/Tsumiji/Stack.swift
@@ -31,3 +31,29 @@ struct Stack {
     }
 }
 
+struct ContextStack {
+    var attrributeStack: [AttributeContext] {
+        didSet {
+            if self.attrributeStack.count == 0 {
+                self.attrributeStack.append(.init())
+            }
+        }
+    }
+    
+    mutating func add(attr: AttributeLink) throws {
+        if var previous = self.attrributeStack.first {
+            attr.modify(&previous)
+            self.attrributeStack.insert(previous, at: 0)
+        } else {
+            self.attrributeStack.insert(attr.createContext(), at: 0)
+        }
+    }
+    
+    mutating func remove() {
+        self.attrributeStack.remove(at: 0)
+    }
+    
+    func getFirst() -> AttributeContext {
+        self.attrributeStack[0]
+    }
+}
