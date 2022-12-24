2fe87f13fc1f19d5892a1e7d8d6dfbc1ccfbacaa
rrbox <87851278+rrbox@users.noreply.github.com>
2022-12-24 15:27:34 +0900

    add ContextStack を追加しました.

--- Sources/Tsumiji/Stack.swift
+++ Sources/Tsumiji/Stack.swift
@@ -5,31 +5,31 @@
 //  Created by rrbox on 2022/12/22.
 //
 
-struct Stack {
-    var attrributeStack: [Attribute] {
-        didSet {
-            if self.attrributeStack.count == 0 {
-                self.attrributeStack.append([:])
-            }
-        }
-    }
-    
-    mutating func add(attr: Attribute) throws {
-        if let previous = self.attrributeStack.first {
-            self.attrributeStack.insert(attr.merging(previous, uniquingKeysWith: { (a, p) in a}), at: 0)
-        } else {
-            self.attrributeStack.insert(attr, at: 0)
-        }
-    }
-    
-    mutating func remove() {
-        self.attrributeStack.remove(at: 0)
-    }
-    
-    func getFirst() -> Attribute {
-        self.attrributeStack[0]
-    }
-}
+//struct Stack {
+//    var attrributeStack: [Attribute] {
+//        didSet {
+//            if self.attrributeStack.count == 0 {
+//                self.attrributeStack.append([:])
+//            }
+//        }
+//    }
+//    
+//    mutating func add(attr: Attribute) throws {
+//        if let previous = self.attrributeStack.first {
+//            self.attrributeStack.insert(attr.merging(previous, uniquingKeysWith: { (a, p) in a}), at: 0)
+//        } else {
+//            self.attrributeStack.insert(attr, at: 0)
+//        }
+//    }
+//    
+//    mutating func remove() {
+//        self.attrributeStack.remove(at: 0)
+//    }
+//    
+//    func getFirst() -> Attribute {
+//        self.attrributeStack[0]
+//    }
+//}
 
 struct ContextStack {
     var attrributeStack: [AttributeContext] {
@@ -40,7 +40,7 @@ struct ContextStack {
         }
     }
     
-    mutating func add(attr: AttributeLink) throws {
+    mutating func add(attr: AttributeLink) {
         if var previous = self.attrributeStack.first {
             attr.modify(&previous)
             self.attrributeStack.insert(previous, at: 0)
@@ -55,5 +55,9 @@ struct ContextStack {
     
     func getFirst() -> AttributeContext {
         self.attrributeStack[0]
+    }
+    
+    init() {
+        self.attrributeStack = [AttributeContext()]
     }
 }
